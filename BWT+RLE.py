8720b06bb486c2f06f5254eb5f7142193111f7bc
Elisabeth Kalinkina <153385301+lisidron@users.noreply.github.com>
2025-03-27 18:08:22 +0300

    Update BWT+RLE.py

--- BWT+RLE.py
+++ BWT+RLE.py
@@ -17,9 +17,6 @@ compressed_size_enwik7 = get_file_size("decompressed_enwik7.txt")
 ratio_enwik7 = calculate_compression_ratio(original_size_enwik7, compressed_size_enwik7)
 analyze_compression("C:/Users/Elisabeth/PycharmProjects/lab1_AISD/material/enwik7.txt", 'enwik7_bwt+rle_encoded.txt')
 print("Правильность декдирования: ", read_file("C:/Users/Elisabeth/PycharmProjects/lab1_AISD/material/enwik7.txt")==read_file("decompressed_enwik7.txt"))
-if read_file("C:/Users/Elisabeth/PycharmProjects/lab1_AISD/material/enwik7.txt") == read_file("decompressed_enwik7.txt"):
-    counter += 1
-# ---------------------------------------------------------------------------------------------------------------------------------------------------
 print("test2 book.txt")
 original_size_book = get_file_size("C:/Users/Elisabeth/PycharmProjects/lab1_AISD/material/book.txt")
 process_bwt_blocks("C:/Users/Elisabeth/PycharmProjects/lab1_AISD/material/book.txt", "bin_book.bin")
@@ -33,9 +30,6 @@ compressed_size_book = get_file_size("decompressed_book.txt")
 ratio_book = calculate_compression_ratio(original_size_book, compressed_size_book)
 analyze_compression("C:/Users/Elisabeth/PycharmProjects/lab1_AISD/material/book.txt", 'book_bwt+rle_encoded.txt')
 print("Правильность декдирования: ", read_file("C:/Users/Elisabeth/PycharmProjects/lab1_AISD/material/book.txt")==read_file("decompressed_book.txt"))
-if read_file("C:/Users/Elisabeth/PycharmProjects/lab1_AISD/material/book.txt") == read_file("decompressed_book.txt"):
-    counter += 1
-# ---------------------------------------------------------------------------------------------------------------------------------------------------
 print("test3 bin.bin")
 original_size_bin = get_file_size("C:/Users/Elisabeth/PycharmProjects/lab1_AISD/material/test3_bin.bin")
 process_bwt_blocks("C:/Users/Elisabeth/PycharmProjects/lab1_AISD/material/test3_bin.bin", "bin_bin.bin")
@@ -49,9 +43,6 @@ compressed_size_bin = get_file_size("decompressed_bin.txt")
 ratio_bin = calculate_compression_ratio(original_size_bin, compressed_size_bin)
 analyze_compression("C:/Users/Elisabeth/PycharmProjects/lab1_AISD/material/test3_bin.bin", 'bin_bwt+rle_encoded.txt')
 print("Правильность декдирования: ", read_file("C:/Users/Elisabeth/PycharmProjects/lab1_AISD/material/test3_bin.bin")==read_file("decompressed_bin.txt"))
-if read_file("C:/Users/Elisabeth/PycharmProjects/lab1_AISD/material/test3_bin.bin") == read_file("decompressed_bin.txt"):
-    counter += 1
-# -----------------------------------------------------------------------------------------------------------------------------------------------------
 print("test4 img1.png")
 original_size_img1 = get_file_size("C:/Users/Elisabeth/PycharmProjects/lab1_AISD/material/img1.raw")
 process_bwt_blocks("C:/Users/Elisabeth/PycharmProjects/lab1_AISD/material/img1.raw", "bin_img1.bin")
@@ -65,9 +56,6 @@ compressed_size_img1 = get_file_size("decompressed_img1.txt")
 ratio_img1 = calculate_compression_ratio(original_size_img1, compressed_size_img1)
 analyze_compression("C:/Users/Elisabeth/PycharmProjects/lab1_AISD/material/img1.raw", 'img1_bwt+rle_encoded.txt')
 print("Правильность декдирования: ", read_file("C:/Users/Elisabeth/PycharmProjects/lab1_AISD/material/img1.raw")==read_file("decompressed_img1.txt"))
-if read_file("C:/Users/Elisabeth/PycharmProjects/lab1_AISD/material/img1.raw") == read_file("decompressed_img1.txt"):
-    counter += 1
-# -----------------------------------------------------------------------------------------------------------------------------------------------------
 print("test5 img2.png")
 original_size_img2 = get_file_size("C:/Users/Elisabeth/PycharmProjects/lab1_AISD/material/img2.raw")
 process_bwt_blocks("C:/Users/Elisabeth/PycharmProjects/lab1_AISD/material/img2.raw", "bin_img2.bin")
@@ -81,9 +69,6 @@ compressed_size_img2 = get_file_size("decompressed_img2.txt")
 ratio_img2 = calculate_compression_ratio(original_size_img2, compressed_size_img2)
 analyze_compression("C:/Users/Elisabeth/PycharmProjects/lab1_AISD/material/img2.raw", 'img2_bwt+rle_encoded.txt')
 print("Правильность декдирования: ", read_file("C:/Users/Elisabeth/PycharmProjects/lab1_AISD/material/img2.raw")==read_file("decompressed_img2.txt"))
-if read_file("C:/Users/Elisabeth/PycharmProjects/lab1_AISD/material/img2.raw") == read_file("decompressed_img2.txt"):
-    counter += 1
-# ----------------------------------------------------------------------------------------------------------------------------------------------------
 print("test6 img3.png")
 original_size_img3 = get_file_size("C:/Users/Elisabeth/PycharmProjects/lab1_AISD/material/img3.raw")
 process_bwt_blocks("C:/Users/Elisabeth/PycharmProjects/lab1_AISD/material/img3.raw", "bin_img3.bin")
@@ -97,9 +82,3 @@ compressed_size_img3 = get_file_size("decompressed_img3.txt")
 ratio_img3 = calculate_compression_ratio(original_size_img3, compressed_size_img3)
 analyze_compression("C:/Users/Elisabeth/PycharmProjects/lab1_AISD/material/img3.raw", 'img3_bwt+rle_encoded.txt')
 print("Правильность декдирования: ", read_file("C:/Users/Elisabeth/PycharmProjects/lab1_AISD/material/img3.raw")==read_file("decompressed_img3.txt"))
-if read_file("C:/Users/Elisabeth/PycharmProjects/lab1_AISD/material/img3.raw") == read_file("decompressed_img3.txt"):
-    counter += 1
-if counter == 6:
-    print('айлялюлю, все круто')
-else:
-    print("расстреливаем всех")
